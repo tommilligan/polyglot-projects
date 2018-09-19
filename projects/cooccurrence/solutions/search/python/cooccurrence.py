d5fb736e2faf7fdbc6b4b3bb2e89395a3e84f6c4
Tom Milligan <tommilligan@users.noreply.github.com>
2018-09-19 11:35:17 +0100

    lint

--- projects/cooccurrence/solutions/search/python/cooccurrence.py
+++ projects/cooccurrence/solutions/search/python/cooccurrence.py
@@ -1,32 +1,32 @@
 class Cooccurrence:
-  def __init__(self, lines):
-    """
-    Parse and set data on `lookup`, where this is a dict like::
+    def __init__(self, lines):
+        """
+        Parse and set data on `lookup`, where this is a dict like::
 
-      {
-        "ALICE": Set([0]),
-        "BOB": Set([0, 1]),
-        ...
-      }
-    
-    """
-    self.lookup = {}
+            {
+              "ALICE": Set([0]),
+              "BOB": Set([0, 1]),
+              ...
+            }
+        
+        """
+        self.lookup = {}
 
-    for i, line in enumerate(lines):
-      tokens = line.rstrip().split(" ")
-      for token in tokens:
-        indexes = self.lookup.get(token, [])
-        self.lookup[token] = indexes + [i]
+        for i, line in enumerate(lines):
+            tokens = line.rstrip().split(" ")
+            for token in tokens:
+                indexes = self.lookup.get(token, [])
+                self.lookup[token] = indexes + [i]
 
-    # dedupe indexes in each key here
-    for k, v in self.lookup.items():
-      self.lookup[k] = set(v)
+        # dedupe indexes in each key here
+        for k, v in self.lookup.items():
+            self.lookup[k] = set(v)
 
-  def get(self, *tokens):
-    """
-    Retrieve the intersection of token
-    """
-    indexes = [self.lookup.get(token, set()) for token in tokens]
-    union = set.intersection(*indexes)
-    return len(union)
+    def get(self, *tokens):
+        """
+        Retrieve the number of lines that contain all n tokens
+        """
+        indexes = [self.lookup.get(token, set()) for token in tokens]
+        union = set.intersection(*indexes)
+        return len(union)
 
